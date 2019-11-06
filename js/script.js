9a747ca5bed9510e749fd30daafc8adaba2dcd0e
Nikita <knocky4u@gmail.com>
2019-11-06 23:49:35 +0600

    FIx

--- js/script.js
+++ js/script.js
@@ -5,43 +5,44 @@ var close = document.querySelector('.modal-close');
 var firstBtn = document.querySelector('.first-btn');
 var secondBtn = document.querySelector('.second-btn');
 var thirdBtn = document.querySelector('.third-btn');
-var body = document.querySelector('body');
+var body = document.querySelector('.wrapper');
 
 var iceCream = document.querySelector('.ice-cream-bg');
-var lightBg = document.querySelector('body');
+var lightBg = document.querySelector('.wrapper');
 
 var textBg = document.querySelector('.text-bg');
 
-link.addEventListener('click', function(evt) {
+link.addEventListener('click', function (evt) {
   evt.preventDefault();
   popup.classList.add('modal');
 });
 
-close.addEventListener('click', function(evt) {
+close.addEventListener('click', function (evt) {
   evt.preventDefault();
   popup.classList.remove('modal');
 });
 
-firstBtn.addEventListener('click', function(evt) {
+firstBtn.addEventListener('click', function (evt) {
   evt.preventDefault();
-  body.style.backgroundColor = '#849d8f';
+  body.style.backgroundColor = "#849d8f";
   iceCream.style.backgroundImage = "url('img/ice_cream.png')";
   lightBg.style.backgroundImage = "url('img/background-icecream-light.png')";
   textBg.innerHTML = 'Крем-брюле и пломбир с малиновым джемом';
+  firstBtn.style.backgroundColor = "#fff";
 });
 
-secondBtn.addEventListener('click', function(evt) {
+secondBtn.addEventListener('click', function (evt) {
   evt.preventDefault();
-  body.style.backgroundColor = '#8996a6';
+  body.style.backgroundColor = "#8996a6";
   iceCream.style.backgroundImage = "url('img/screen2.png')";
   lightBg.style.backgroundImage = "url('img/screen2-light.png')";
   textBg.innerHTML = 'Шоколадный пломбир и лимонный сорбет';
   firstBtn.style.backgroundColor = null;
 });
 
-thirdBtn.addEventListener('click', function(evt) {
+thirdBtn.addEventListener('click', function (evt) {
   evt.preventDefault();
-  body.style.backgroundColor = '#9d8b84';
+  body.style.backgroundColor = "#9d8b84";
   iceCream.style.backgroundImage = "url('img/screen3.png')";
   lightBg.style.backgroundImage = "url('img/screen3-light.png')";
   textBg.innerHTML = 'Пломбир с помадкой и клубничный щербет';
